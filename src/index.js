8ee87a4a454da4ed054b9b97d50e93a6aeb0802b
tosada <tomoya.a.osada@gmail.com>
2017-01-19 01:41:00 +0900

    first commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import CalculatorContainer from './containers/CalculatorContainer'
+import './index.css';
+
+ReactDOM.render(
+  <CalculatorContainer />,
+  document.getElementById('root')
+);
