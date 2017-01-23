0d76b15d881f2a9384499a22e0fd0730fff4c038
tosada <tomoya.a.osada@gmail.com>
2017-01-24 01:32:36 +0900

    Updates

--- src/components/Result.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Result = ({ result }) => (
-  <div>
-    Result: <span>{ result }</span>
-  </div>
-)
-
-export default Result
--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import calculator from './calculator.js'
-
-const reducer = combineReducers({
-  calculator,
-});
-
-export default reducer;
