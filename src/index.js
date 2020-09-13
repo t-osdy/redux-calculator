c88dbe8b5a169f293aaa92faf9694a0e23061906
t-osdy <tomoya.a.osada@gmail.com>
2020-09-13 22:44:12 +0900

    container

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import CalculatorContainer from './containers/CalculatorContainer'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <CalculatorContainer />
   </React.StrictMode>,
   document.getElementById('root')
 );
